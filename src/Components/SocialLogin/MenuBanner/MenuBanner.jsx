0173339fab7de6523dcc26efcb3daff133d3edef
Ahammad204 <Ahammadcode@gmail.com>
2024-03-08 15:59:51 +0600

    subassignment page updated

--- src/Components/SocialLogin/MenuBanner/MenuBanner.jsx
+++ src/Components/SocialLogin/MenuBanner/MenuBanner.jsx
@@ -15,7 +15,7 @@ const MenuBanner = ({ title, bgImg }) => {
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
                     <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <p className="mb-5">Find All Available Assignment boost your Skill</p>
 
                 </div>
             </div>
--- src/Pages/SubmittedAssignment/SubmittedAssignment.jsx
+++ src/Pages/SubmittedAssignment/SubmittedAssignment.jsx
@@ -5,6 +5,7 @@ import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { useState } from "react";
 import { Pagination } from "@mui/material";
 import useSubmitAssignment from "../../Hooks/useSubmitAssignment";
+import { FaHandHoldingDollar } from "react-icons/fa6";
 
 
 const MyAddedAssignment = () => {
@@ -64,6 +65,7 @@ const MyAddedAssignment = () => {
                             <th className="px-4 py-2">Status</th>
                             <th className="px-4 py-2">Marks</th>
                             <th className="px-4 py-2">Delete</th>
+                            <th className="px-4 py-2">Details</th>
                             {/* <th className="px-4 py-2">Adopted</th> */}
                         </tr>
                     </thead>
@@ -85,7 +87,7 @@ const MyAddedAssignment = () => {
                                 <td className="border text-center uppercase px-4 py-2">{assignment?.status}</td>
                                 <td className="border text-center uppercase px-4 py-2">{assignment?.marks}</td>
 
-                             
+
                                 <td className="border text-center px-4 py-2">
                                     <button
                                         onClick={() => handleDelete(assignment?._id)}
@@ -93,6 +95,101 @@ const MyAddedAssignment = () => {
                                     >
                                         <FaTrashAlt className="text-blue-400"></FaTrashAlt>
                                     </button>
+                                </td>
+
+                                <td>
+                                    <button
+                                        onClick={() => {
+                                            document.getElementById(`my_modal_${assignment._id}`).showModal();
+                                        }}
+                                        className="btn btn-ghost btn-lg">
+                                        <FaHandHoldingDollar className="text-blue-400" />
+                                    </button>
+
+
+                                    {/* Open the modal using document.getElementById('ID').showModal() method */}
+                                    <dialog id={`my_modal_${assignment._id}`} className="modal text-slate-500">
+                                        <div className="modal-box">
+                                            <form method="dialog">
+                                                {/* if there is a button in form, it will close the modal */}
+                                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                                            </form>
+                                            <form className="mt-4">
+
+
+
+                                                {/* Name and Email row */}
+                                                <div className="md:flex mb-8">
+                                                    <div className="form-control md:w-1/2">
+                                                        <label className="label">
+                                                            <span className="label-text">Examinee Name</span>
+                                                        </label>
+                                                        <label className="input-group">
+
+                                                            <input type="text" disabled defaultValue={assignment?.names} required name="names" placeholder="Enter Your Full Name" className="input input-bordered w-full" />
+                                                        </label>
+                                                    </div>
+                                                    <div className="form-control md:w-1/2 ml-4">
+                                                        <label className="label">
+                                                            <span className="label-text">Examinee Email</span>
+                                                        </label>
+                                                        <label className="input-group">
+
+                                                            <input type="email" disabled defaultValue={assignment?.email} required name="email" placeholder="Enter Your Email" className="input input-bordered w-full" />
+                                                        </label>
+                                                    </div>
+                                                </div>
+
+                                                {/* Assignment and note row */}
+                                                <div className="md:flex mb-8">
+                                                    <div className="form-control md:w-1/2">
+                                                        <label className="label">
+                                                            <span className="label-text">Assignment</span>
+                                                        </label>
+                                                        <label className="input-group">
+
+                                                            <textarea type="text" disabled defaultValue={assignment?.AssignmentSubmit} required name="assignmentSubmit" placeholder="Assignment Submit" className="input input-bordered w-full" />
+                                                        </label>
+                                                    </div>
+                                                    <div className="form-control md:w-1/2 ml-4">
+                                                        <label className="label">
+                                                            <span className="label-text">Note</span>
+                                                        </label>
+                                                        <label className="input-group">
+
+                                                            <textarea type="text" disabled defaultValue={assignment?.
+                                                                shortNote} required name="shotNote" placeholder="Short Note" className="input input-bordered w-full" />
+                                                        </label>
+                                                    </div>
+                                                </div>
+
+                                                {/* Mark and Feedback row */}
+                                                <div className="md:flex mb-8">
+                                                    <div className="form-control md:w-1/2">
+                                                        <label className="label">
+                                                            <span className="label-text">Assignment</span>
+                                                        </label>
+                                                        <label className="input-group">
+
+                                                            <textarea type="text" disabled defaultValue={assignment?.feedback} required name="assignmentSubmit" placeholder="Assignment Submit" className="input input-bordered w-full" />
+                                                        </label>
+                                                    </div>
+                                                    <div className="form-control md:w-1/2 ml-4">
+                                                        <label className="label">
+                                                            <span className="label-text">Note</span>
+                                                        </label>
+                                                        <label className="input-group">
+
+                                                            <input type="number" disabled defaultValue={assignment?.marks} required name="shotNote" placeholder="Short Note" className="input input-bordered w-full" />
+                                                        </label>
+                                                    </div>
+                                                </div>
+
+                                            </form>
+                                        </div>
+                                    </dialog>
+
+
                                 </td>
 
                             </tr>
